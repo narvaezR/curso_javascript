6f24b08b0e00813cb152d7cc13a6936c1df7ddac
Ricardo Narvaez Jaramillo <rricardo.n18@gmail.com>
2022-04-28 20:28:53 -0500

    Actualización

--- modulo1/clase2.js
+++ modulo1/clase2.js
@@ -5,5 +5,41 @@ var EdadRicardo = 23
 console.log(EdadRicardo)
 
 //Tipos de datos 
-//Numerico 
+//Numerico (integer, number, int)
 var numero  = 0
+var numero2 = 10
+var numero3 = 3.1416
+//texto (string, text, )
+var texto = "hola"
+var texto1 = "Hola mundo"
+var texto2 = "10"
+//booleanos, (boot, bolean)
+var boolean = true //verdadera
+var boolean2 = false // falso 
+//Arreglos, listas (array) se definen entre corchetes y se separan con coma, almacenan lista de numeros, texto, boolean y otras variables  
+var lista1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+var lista2 = ["andenson", "ricardo", "Juliana"]
+var lista3 = [true, false, true]
+var lista4 = [1, true, "hola"]
+var listavacia = []
+var listavariables =[numero, texto1, boolean2]
+var elemento4 = lista1 [3]// elemento4
+//constantes
+const pi = 3.1416
+//modificadores 
+var numero4 = 100
+var copianumero4 = numero4
+var numero4 = 100
+numero4 = numero4-10
+alert (numero4)
+numero4 = numero4 + pi // numero4= 100 + 3.1416
+
+//texto 
+var hola ="hola"
+var como = "como"
+var estas = "estas"
+var saludo = hola + como + estas
+
+var saludo2 = ${hola} ${como} ${estas}
+alert (saludo2)
+alert(saludo)
---
+++ modulo1/clase3.js
@@ -0,0 +1,70 @@
+//datos nulos e indefinidas
+const constanteNula = null
+var constanteIndefinida = undefined
+//Modificadores de listas 
+var lista1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+//agregar un elemento a una lista 
+lista1.push(11)
+console.log(lista)
+//sacar un elemento de una lista 
+lista1.pop()
+console.log(lista1)
+//modificadores booleanos 
+var verdadera = true
+var falsa = false
+verdadera = !verdadera
+falsa = !falsa
+//objetos en javascritp 
+var persona = {
+    Nombre: "Ricardo Narvaez",
+    edad: 22,
+    nacionalidad: "Colombiano",
+    colorPreferido: "Negro"
+}
+//modificadores de obejtos en Js
+//imprimir propiedades de un objeto 
+console.log(persona.Nombre)
+//Guardar 
+const NombrePersona = persona.Nombre
+console.log(NombrePersona)
+// agregar más propiedades a un objeto 
+persona.comidadPreferida = "Hamburguesa"
+console.log(persona)
+//eliminar una propiedad de un objeto 
+delete persona.edad
+console.log (persona)
+//COMPARADORES 
+//Mayor y menor que
+4>3
+4<3
+//igual 
+       // el = esta asignado para las variables y constantes, en comparadores se utiliza 3 = (===) 
+4 === 4
+"hola" ==== "hola"
+true === true
+true === false
+true === !false 
+// diferencia 
+ 4 !== 3 
+ 4 !== 4
+          // funciona con textos, boleans, nulls 
+
+//OPERADORES LÓGICOS 
+// And (y) &&
+// para que un And sea verdadero true los valores de verdad que tiene a su derecha
+//verdaderos 
+true && true // true 
+// Or | ó
+// para que un Or sea verdadero alguno de los valores a su izquierda o dereche deben ser verdaderos 
+true || true
+true || false 
+false || false 
+true || true || false 
+
+// combinar 
+
+4 > 3 || 5<2
+
+
+
+
