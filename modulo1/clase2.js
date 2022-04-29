4f2b4fc1fc175d048173d4807940f7be057a6a0c
Ricardo Narvaez Jaramillo <rricardo.n18@gmail.com>
2022-04-28 19:17:48 -0500

    Carga de las tres primeras clases

---
+++ modulo1/clase2.js
@@ -0,0 +1,9 @@
+// esta linea es un comentario para el computador 
+//vari
+var EdadRicardo = 23
+// para poder imprrimir una variable necesitamos hacer console.log
+console.log(EdadRicardo)
+
+//Tipos de datos 
+//Numerico 
+var numero  = 0
